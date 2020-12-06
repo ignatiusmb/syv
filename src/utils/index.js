5cc1549b1de674e8398268ffc4cec7f68753aa00
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-12-06 13:07:13 +0700

    remove utils folder

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export function capitalize(text, lower) {
-	return (lower ? text.toLowerCase() : text).replace(/(?:^|\s)\S/g, function (a) {
-		return a.toUpperCase();
-	});
-}
-
-export const checkNum = (text) => (isNaN(text) ? text : parseInt(text));
