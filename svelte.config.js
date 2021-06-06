f4eac64eb7995ec5d833ce4239740a82870f03f0
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-06-06 23:14:42 +0700

    add svelte-kit packages and files

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess({
+		defaults: { script: 'typescript' },
+	}),
+	kit: {
+		adapter: adapter(),
+		vite: {
+			optimizeDeps: {
+				exclude: ['marqua'],
+				include: ['markdown-it'],
+			},
+			ssr: {
+				noExternal: ['mauss'],
+			},
+		},
+	},
+};
+
+export default config;
