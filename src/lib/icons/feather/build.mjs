9415d64f260e2e24704715e6e985fa4e9cce102a
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-08-17 21:52:23 +0700

    read prettier config from mauss

--- src/lib/icons/feather/build.mjs
+++ src/lib/icons/feather/build.mjs
@@ -1,6 +1,7 @@
 import { icons as feather } from 'feather-icons';
+import { existsSync, readFileSync } from 'fs';
 import { writeFile } from 'fs/promises';
-import { format } from 'prettier';
+import prettier from 'prettier';
 
 const pascalCase = (w) => `${w[0].toUpperCase()}${w.slice(1).toLowerCase()}`;
 const generate = (icon) => `<script>
@@ -29,17 +30,30 @@ const generate = (icon) => `<script>
 
 export default {
 	async build() {
+		const config = await this.config();
+
 		let idx = '';
 		let dts = `import { SvelteComponentTyped } from 'svelte';\nexport class BaseIcon extends SvelteComponentTyped<{\n\tclass?: string;\n\tsize?: number | string;\n\tfloat?: number | string;\n\tcolor?: string;\n}> {}\n`;
 		for (const kebab in feather) {
 			const pascal = kebab.replace(/\w+/g, pascalCase).replace(/-/g, '');
-			writeFile(`./feather/${pascal}.svelte`, format(generate(kebab)));
+			const formatted = prettier.format(generate(kebab), config);
+			writeFile(`./feather/${pascal}.svelte`, formatted);
 			idx += `export { default as ${pascal} } from './${pascal}.svelte';\n`;
 			dts += `export class ${pascal} extends BaseIcon {}\n`;
 		}
+
 		await Promise.all([
 			writeFile('./feather/index.js', idx),
 			writeFile('./feather/index.d.ts', dts),
 		]);
+	},
+	async config() {
+		let path = 'node_modules';
+		while (!existsSync(path)) path = `../${path}`;
+		const { overrides, ...workspace } = JSON.parse(readFileSync(`${path}/mauss/prettier.json`));
+		const { options: svelte } = overrides.find(({ files }) => files[0].endsWith('.svelte'));
+
+		delete workspace['$schema'];
+		return { ...workspace, ...svelte };
 	},
 };
