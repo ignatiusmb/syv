954f18be765a695d86de8b00083ddcae6c918cb9
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-09-05 23:23:31 +0700

    searchbar usage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<script context="module" lang="ts">
+<script context="module">
 	// import type { Load } from '@sveltejs/kit';
 	// export const load: Load = async ({ fetch }) => {
 	// 	const sections = await fetch('/docs.json').then((r) => r.json());
@@ -9,7 +9,32 @@
 <script>
 	// export let sections;
 	// import Docs from '$lib/internal/Docs.svelte';
+
+	import SearchBar from '$lib/core/SearchBar.svelte';
 </script>
 
 <!-- <Docs {sections} repo="ignatiusmb/aqua" /> -->
-<h1>Hello</h1>
+<main>
+	<SearchBar
+		icon
+		placeholder="検索する（例：ログイン、顧客登録、パスワードの初期化）"
+		class="max-w-2xl w-full rounded-xl"
+		filters
+	/>
+</main>
+
+<style>
+	main {
+		--fg-surface: #818181;
+		--bg-overlay: #ffffff;
+	}
+	main {
+		max-width: 86rem;
+		width: 100%;
+		display: grid;
+		justify-items: center;
+
+		padding: 2em;
+		margin: 0 auto;
+	}
+</style>
