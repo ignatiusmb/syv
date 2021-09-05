9613683d7a1bc16af62ce843e824afed2fef9d2e
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-09-05 23:25:07 +0700

    chore: asynchronously load config

--- src/lib/icons/feather/build.mjs
+++ src/lib/icons/feather/build.mjs
@@ -30,13 +30,13 @@ const generate = (icon) => `<script>
 
 export default {
 	async build() {
-		const config = await this.config();
+		const config = this.config();
 
 		let idx = '';
 		let dts = `import { SvelteComponentTyped } from 'svelte';\nexport class BaseIcon extends SvelteComponentTyped<{\n\tclass?: string;\n\tsize?: number | string;\n\tweight?: number | string;\n\tcolor?: string;\n}> {}\n`;
 		for (const kebab in feather) {
 			const pascal = kebab.replace(/\w+/g, pascalCase).replace(/-/g, '');
-			const formatted = prettier.format(generate(kebab), config);
+			const formatted = prettier.format(generate(kebab), await config);
 			writeFile(`./feather/${pascal}.svelte`, formatted);
 			idx += `export { default as ${pascal} } from './${pascal}.svelte';\n`;
 			dts += `export class ${pascal} extends BaseIcon {}\n`;
