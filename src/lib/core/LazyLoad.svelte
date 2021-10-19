9aaa267f004ac271aa5d3558213456d895386606
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-10-19 22:20:17 +0700

    change file -> files, let:loaded returns an array

--- src/lib/core/LazyLoad.svelte
+++ src/lib/core/LazyLoad.svelte
@@ -1,13 +1,15 @@
 <script>
-	export let file;
+	export let files;
 	export let when = true;
 
-	let component;
-	$: if (when && !component) component = file();
+	let promises;
+	$: if (when && !promises && files.length) {
+		promises = files.map((file) => file());
+	}
 </script>
 
-{#if component && when}
-	{#await component then { default: loaded }}
-		<slot {loaded} />
+{#if when && promises && promises.length}
+	{#await Promise.all(promises) then components}
+		<slot loaded={components.map((c) => c.default)} />
 	{/await}
 {/if}
