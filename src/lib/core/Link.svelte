d1764173074a292d6f9ed20f4ba002be17a2625c
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-12-16 21:17:38 +0700

    docs: annotate `core/Link.svelte` props (#55)

--- src/lib/core/Link.svelte
+++ src/lib/core/Link.svelte
@@ -1,24 +1,33 @@
 <script>
 	export let href = '';
+	/** passed through to aria-label */
 	export let label = '';
+	/** sets rel to external and disable sveltekit:prefetch */
 	export let external = /^https?:\/\//.test(href);
+	/** adds noreferrer to rel, only applied when external is true */
+	export let refer = false;
+	/** sets sveltekit:noscroll value */
+	export let noscroll = false;
+	/** sets download attribute */
 	export let download = false;
+	/** sets target as '_blank' */
 	export let newTab = false;
+
+	/** adds inherit class to element */
 	export let inherit = false;
+	/** adds invert class to element */
 	export let invert = false;
 
-	export let noscroll = false;
-	export let refer = false;
 	export { className as class };
 	let className = '';
 </script>
 
 <a
 	{href}
-	sveltekit:noscroll={noscroll || undefined}
-	sveltekit:prefetch={!external || undefined}
-	rel={external ? `external noopener${refer ? '' : ' noreferrer'}` : undefined}
 	aria-label={label || undefined}
+	rel={external ? `external noopener${refer ? '' : ' noreferrer'}` : undefined}
+	sveltekit:prefetch={!external || undefined}
+	sveltekit:noscroll={noscroll || undefined}
 	download={download || undefined}
 	target={newTab ? '_blank' : undefined}
 	class:invert
