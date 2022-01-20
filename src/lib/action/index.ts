ee1257bfe1cd6248a23af8e99abe68b6066113c2
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-01-20 14:49:49 +0700

    node autofocus

--- src/lib/action/index.ts
+++ src/lib/action/index.ts
@@ -1,8 +1,16 @@
 export * as click from './click';
 
+// TODO replace with https://github.com/sveltejs/svelte/pull/7121
 export interface Action<Parameters = any, Element = HTMLElement> {
 	<Node extends Element>(node: Node, parameters?: Parameters): void | {
 		update?: (parameters: Parameters) => void;
 		destroy?: () => void;
 	};
 }
+
+export const autofocus: Action<boolean> = (node, when = true) => {
+	if (when) node.focus();
+	return {
+		update: (when) => when && node.focus(),
+	};
+};
