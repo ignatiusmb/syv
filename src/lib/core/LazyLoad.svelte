98340b240fe1871cc0d5cc71671753909810ca9e
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-01-28 13:28:25 +0700

    add loading slot

--- src/lib/core/LazyLoad.svelte
+++ src/lib/core/LazyLoad.svelte
@@ -11,7 +11,9 @@
 </script>
 
 {#if when && promises && promises.length}
-	{#await Promise.all(promises) then components}
+	{#await Promise.all(promises)}
+		<slot name="loading" />
+	{:then components}
 		<slot loaded={components.map((c) => c.default)} />
 	{/await}
 {/if}
