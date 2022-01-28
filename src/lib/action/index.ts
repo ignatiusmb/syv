fd81f4a861a4acc90bd6ac216fe77fa867be868a
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-01-28 13:26:30 +0700

    update action types

--- src/lib/action/index.ts
+++ src/lib/action/index.ts
@@ -1,11 +1,12 @@
 export * as click from './click';
 
 // TODO replace with https://github.com/sveltejs/svelte/pull/7121
+interface ActionReturn<Parameters = any> {
+	update?: (parameters: Parameters) => void;
+	destroy?: () => void;
+}
 export interface Action<Parameters = any, Element = HTMLElement> {
-	<Node extends Element>(node: Node, parameters?: Parameters): void | {
-		update?: (parameters: Parameters) => void;
-		destroy?: () => void;
-	};
+	<Node extends Element>(node: Node, parameters?: Parameters): void | ActionReturn<Parameters>;
 }
 
 /** autofocus element when condition is true */
