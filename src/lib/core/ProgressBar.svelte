ac52a47180f4109d62adc77e4e8f29bf63d349a6
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-05-17 19:46:42 +0700

    annotate variables

--- src/lib/core/ProgressBar.svelte
+++ src/lib/core/ProgressBar.svelte
@@ -2,13 +2,15 @@
 	export { className as class };
 	let className = '';
 
+	/** @type {number} */
 	let y,
 		mounted = typeof window !== 'undefined',
-		innerHeight = mounted && document.body.scrollHeight;
+		/** @type {undefined | number} */
+		innerHeight = mounted ? document.body.scrollHeight : undefined;
 	import { onMount } from 'svelte';
 	onMount(() => (mounted = true));
 	$: scrolled = mounted ? document.body.scrollHeight : y;
-	$: progress = (y / (scrolled - innerHeight)) * 100;
+	$: progress = (y / (scrolled - (innerHeight || 0))) * 100;
 </script>
 
 <svelte:window bind:scrollY={y} bind:innerHeight />
