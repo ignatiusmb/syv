21a1d1e2685298966bbe09063919daf907b521f5
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-05-24 23:24:07 +0800

    feat: annotate `core/Video.svelte` (#73)

--- src/lib/core/Video.svelte
+++ src/lib/core/Video.svelte
@@ -1,11 +1,14 @@
 <script>
+	/** @type {string | string[]} */
 	export let src = '';
 	/* Passed Values */
 	export let autoplay = false;
 	export let controls = false;
 	export let loop = false;
 	// export let poster = '';
+	/** @type {null | number} */
 	export let width = null;
+	/** @type {null | number} */
 	export let height = null;
 	/* Binding Values */
 	export let buffered = undefined;
@@ -14,10 +17,19 @@
 	export let muted = false;
 	export { className as class };
 	let className = '';
+	/** @type {HTMLVideoElement} */
 	let video; // Bind Video Element
 
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
+
+	/**
+	 * @typedef {'contact' | 'leave'} DispatchedKind
+	 * @typedef {MouseEvent | TouchEvent} PossibleEvents
+	 */
+
+	/** @type {(k: DispatchedKind) => (ev: PossibleEvents) => void} */
+	const handler = (k) => (event) => dispatch(k, { self: video, event });
 </script>
 
 <video
@@ -26,21 +38,21 @@
 	{autoplay}
 	{controls}
 	{loop}
+	class="syv-core-video {className}"
 	bind:this={video}
 	bind:buffered
 	bind:currentTime
 	bind:duration
 	bind:muted
-	on:mouseenter={(event) => dispatch('contact', { self: video, event })}
-	on:mouseleave={(event) => dispatch('leave', { self: video, event })}
-	on:touchstart={(event) => dispatch('contact', { self: video, event })}
-	on:touchend={(event) => dispatch('leave', { self: video, event })}
-	on:touchcancel={(event) => dispatch('leave', { self: video, event })}
-	class="syv-core-video {className}"
+	on:mouseenter={handler('contact')}
+	on:mouseleave={handler('leave')}
+	on:touchstart={handler('contact')}
+	on:touchend={handler('leave')}
+	on:touchcancel={handler('leave')}
 >
 	{#if typeof src === 'string'}
 		<source {src} type="video/{src.slice(src.lastIndexOf('.') + 1)}" />
-	{:else if src instanceof Array}
+	{:else if src.length}
 		{#each src as vid}
 			<source src={vid} type="video/{vid.slice(vid.lastIndexOf('.') + 1)}" />
 		{/each}
