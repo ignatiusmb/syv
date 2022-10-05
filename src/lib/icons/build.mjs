3a7ad6a648ea3ce6efca83b99e54fc1b9a6e7aa8
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-10-05 18:28:19 +0700

    silly mistake

--- src/lib/icons/build.mjs
+++ src/lib/icons/build.mjs
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 fs.readdirSync('.').forEach(async (path) => {
 	if (!fs.statSync(path).isDirectory()) return;
-	if (!fs.existsSync(`./${path}/index.js`)) return;
+	if (fs.existsSync(`./${path}/index.js`)) return;
 	const { build } = await import(`./${path}/build.mjs`);
 	build();
 });
