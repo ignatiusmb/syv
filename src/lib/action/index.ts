a6cda7f4a28eef7078c3daed9c27bde3ecb4c2b7
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-10-06 15:04:43 +0700

    fix(`action`): vertical padding for autoresize

--- src/lib/action/index.ts
+++ src/lib/action/index.ts
@@ -9,12 +9,12 @@ export const autofocus: HTMLAction<boolean> = (node, when = true) => (
 
 /** automatically expand/shrink `textarea` height according to content  */
 export const autoresize: Action<HTMLTextAreaElement> = (node) => {
-	const { paddingBlock } = getComputedStyle(node);
-	const computed = +paddingBlock.slice(0, -2);
-	let memory = node.scrollHeight + computed;
+	const { paddingTop: pt, paddingBottom: pb } = getComputedStyle(node);
+	const vertical = parseFloat(pt.slice(0, -2)) + parseFloat(pb.slice(0, -2));
+	let memory = node.scrollHeight + vertical;
 	const receiver = () => {
-		if (node.scrollHeight <= memory) return;
-		memory = node.scrollHeight + computed;
+		if (node.scrollHeight <= memory + vertical) return;
+		memory = node.scrollHeight + vertical;
 		node.style.setProperty('height', `${memory}px`);
 	};
 
