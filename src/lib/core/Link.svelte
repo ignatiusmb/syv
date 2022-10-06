e77c8a615e72d2be4034045c3ba0bc115b33f3a2
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-10-06 17:35:41 +0700

    feat(`core/Link`): support SK RC version

--- src/lib/core/Link.svelte
+++ src/lib/core/Link.svelte
@@ -2,21 +2,19 @@
 	export let href = '';
 	/** passed through to aria-label */
 	export let label = '';
-	/** sets rel to external and disable sveltekit:prefetch */
-	export let external = /^https?:\/\//.test(href);
 	/** adds noreferrer to rel, only applied when external is true */
 	export let refer = false;
-	/** sets sveltekit:noscroll value */
-	export let noscroll = false;
 	/** sets download attribute */
 	export let download = false;
 	/** sets target as '_blank' */
 	export let newTab = false;
 
-	/** adds inherit class to element */
-	export let inherit = false;
-	/** adds invert class to element */
-	export let invert = false;
+	/** sets rel to external and disable data-sveltekit-prefetch */
+	export let external = /^https?:\/\//.test(href);
+	/** @type {boolean | 'off'} - sets data-sveltekit-prefetch value */
+	export let prefetch = !external;
+	/** @type {boolean | 'off'} - sets data-sveltekit-noscroll value */
+	export let noscroll = false;
 
 	export { className as class };
 	let className = '';
@@ -26,12 +24,10 @@
 	{href}
 	aria-label={label || undefined}
 	rel={external ? `external noopener${refer ? '' : ' noreferrer'}` : undefined}
-	sveltekit:prefetch={!external || undefined}
-	sveltekit:noscroll={noscroll || undefined}
+	data-sveltekit-prefetch={prefetch || undefined}
+	data-sveltekit-noscroll={noscroll || undefined}
 	download={download || undefined}
 	target={newTab ? '_blank' : undefined}
-	class:invert
-	class:inherit
 	class:disabled={!href}
 	class="syv-core-link {className}"
 >
@@ -44,11 +40,5 @@
 	}
 	a.disabled {
 		cursor: not-allowed;
-	}
-	a.inherit {
-		color: inherit !important;
-	}
-	a.invert:hover {
-		filter: invert() hue-rotate(180deg);
 	}
 </style>
