47f5098bb58cac2dcd05f6abf287ff66215babbd
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-10-07 09:11:33 +0700

    feat: annotate `core/ProgressBar.svelte` (#81)

--- src/lib/core/ProgressBar.svelte
+++ src/lib/core/ProgressBar.svelte
@@ -1,27 +1,34 @@
 <script>
+	export let z = 3;
+	/** height in css unit */
+	export let height = '1rem';
 	export { className as class };
 	let className = '';
 
-	let y,
-		mounted = typeof window !== 'undefined',
-		innerHeight = mounted && document.body.scrollHeight;
-	import { onMount } from 'svelte';
-	onMount(() => (mounted = true));
-	$: scrolled = mounted ? document.body.scrollHeight : y;
-	$: progress = (y / (scrolled - innerHeight)) * 100;
+	import { mounted } from '../store';
+
+	let scrollY = $mounted ? document.body.scrollTop : 0;
+	let innerHeight = $mounted ? document.body.scrollHeight : 1;
+
+	$: scrolled = $mounted ? document.body.scrollHeight : scrollY;
+	$: progress = (scrollY / (scrolled - innerHeight)) * 100;
 </script>
 
-<svelte:window bind:scrollY={y} bind:innerHeight />
-<div class="syv-core-progress-bar {className}" style="transform:translateX({progress}%)" />
+<svelte:window bind:scrollY bind:innerHeight />
+
+<div
+	class="syv-core-progress-bar {className}"
+	style:height
+	style:z-index={z}
+	style:transform="translateX({progress}%)"
+/>
 
 <style>
 	div {
-		z-index: 3;
+		width: 100%;
 		position: fixed;
 		top: 0;
 		left: -100%;
-		width: 100%;
-		height: 0.25em;
 		background-color: #990000;
 	}
 </style>
