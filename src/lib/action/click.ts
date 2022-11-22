cd27b78d493e014d7a852cad147c04775aa87c5c
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-11-22 21:09:34 +0700

    feat: click to copy action

--- src/lib/action/click.ts
+++ src/lib/action/click.ts
@@ -1,12 +1,29 @@
 import type { HTMLAction } from './types.js';
 import { noop } from '../utils.js';
 
+export const copy: HTMLAction<{ text?: string }> = (node, opts = {}) => {
+	function write() {
+		if (!opts.text) return;
+		navigator.clipboard.writeText(opts.text);
+	}
+
+	node.addEventListener('click', write, true);
+	return {
+		update({ text: updated = '' }) {
+			opts.text = updated;
+		},
+		destroy() {
+			node.removeEventListener('click', write, true);
+		},
+	};
+};
+
 /** determine if there is click event outside */
 export const outside: HTMLAction<(event: MouseEvent) => void> = (node, callback) => {
-	const clicked = (event: MouseEvent) => {
+	function clicked(event: MouseEvent) {
 		if (!node || !callback || event.defaultPrevented) return;
 		if (!node.contains(event.target as Node)) callback(event);
-	};
+	}
 
 	document.addEventListener('click', clicked, true);
 	return {
@@ -23,12 +40,12 @@ export const hold: HTMLAction<{
 }> = (node, { duration = 2000, invoke = noop } = {}) => {
 	let timer: NodeJS.Timeout;
 
-	const press = () => {
+	function press() {
 		timer = setTimeout(invoke, duration);
-	};
-	const release = () => {
+	}
+	function release() {
 		if (timer) clearTimeout(timer);
-	};
+	}
 
 	node.addEventListener('mousedown', press);
 	node.addEventListener('mouseup', release);
