8dc44d769ce8e7bcc5f0b36924fec8b6f18bf874
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-11-30 12:33:49 +0700

    chore: clean up exports map (#107)

--- svelte.config.js
+++ svelte.config.js
@@ -8,11 +8,17 @@ const config = {
 		adapter: adapter(),
 	},
 	package: {
-		exports: (filepath) => !filepath.startsWith('.') && !filepath.startsWith('internal/lib'),
-		files: (filepath) =>
-			!filepath.endsWith('build.mjs') &&
-			!filepath.startsWith('.svelte-kit') &&
-			filepath !== 'tsconfig.json',
+		exports(filepath) {
+			if (filepath.startsWith('.')) return false; // dotfiles are private
+			if (filepath.endsWith('.d.ts')) return false; // handwritten types
+			if (filepath.includes('/lib/')) return false; // internal components
+			if (filepath.startsWith('icons/')) return !filepath.endsWith('.svelte');
+			return true;
+		},
+		files(filepath) {
+			if (filepath.endsWith('build.mjs')) return false; // build script
+			return filepath !== 'tsconfig.json';
+		},
 	},
 };
 
