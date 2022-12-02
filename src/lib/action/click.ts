e110b9c906b87ba43eb568e2add2b4cf24369fc5
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-12-02 18:13:04 +0700

    feat: click to copy action (#105)

--- src/lib/action/click.ts
+++ src/lib/action/click.ts
@@ -1,16 +1,40 @@
 import type { HTMLAction } from './types.js';
+import { clipboard } from 'mauss/web';
 import { noop } from '../utils.js';
+
+export const copy: HTMLAction<{
+	data?: string | Blob;
+	handler?: Parameters<typeof clipboard.copy>[1];
+}> = (node, { data, handler } = {}) => {
+	function write() {
+		if (!data) return;
+		if (typeof data === 'string') clipboard.copy(data, handler);
+		else clipboard.copy(clipboard.item(data.type, data), handler);
+	}
+
+	node.addEventListener('click', write, true);
+	return {
+		update({ data: updated = '' }) {
+			data = updated;
+		},
+		destroy() {
+			node.removeEventListener('click', write, true);
+		},
+	};
+};
 
 /** determine if there is click event outside */
 export const outside: HTMLAction<(event: MouseEvent) => void> = (node, callback) => {
-	const clicked = (event: MouseEvent) => {
+	function clicked(event: MouseEvent) {
 		if (!node || !callback || event.defaultPrevented) return;
 		if (!node.contains(event.target as Node)) callback(event);
-	};
+	}
 
 	document.addEventListener('click', clicked, true);
 	return {
-		destroy: () => document.removeEventListener('click', clicked, true),
+		destroy() {
+			document.removeEventListener('click', clicked, true);
+		},
 	};
 };
 
@@ -23,12 +47,12 @@ export const hold: HTMLAction<{
 }> = (node, { duration = 2000, invoke = noop } = {}) => {
 	let timer: NodeJS.Timeout;
 
-	const press = () => {
+	function press() {
 		timer = setTimeout(invoke, duration);
-	};
-	const release = () => {
+	}
+	function release() {
 		if (timer) clearTimeout(timer);
-	};
+	}
 
 	node.addEventListener('mousedown', press);
 	node.addEventListener('mouseup', release);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { autoresize } from '$lib/action';
-	import { outside, hold } from '$lib/action/click';
+	import { outside, copy } from '$lib/action/click';
 
 	import { LazyLoad } from '$lib';
+
+	let value = '';
 </script>
 
 <main use:outside={() => {}}>
-	<a href="/" use:hold>Home</a>
+	<textarea bind:value use:autoresize />
 
-	<textarea use:autoresize />
+	<button use:copy={{ data: value }}>Copy to Clipboard</button>
 
 	<LazyLoad files={[() => import('$lib/core/Dialog.svelte')]} let:loaded>
 		<svelte:component this={loaded[0]} />
