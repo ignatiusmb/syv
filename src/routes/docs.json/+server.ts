e2727b2e7ce453ea015a4bd0e62629f9b2aebeb5
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-02-17 18:09:15 +0800

    update marqua usage

--- src/routes/docs.json/+server.ts
+++ src/routes/docs.json/+server.ts
@@ -1,22 +1,21 @@
 import { json } from '@sveltejs/kit';
-import { forge, traverse } from 'marqua';
+import { traverse } from 'marqua/fs';
 import { version } from '../../../package.json';
 
-type Metadata = Record<'title', string>;
-type Section = Record<'index' | 'slug' | 'content' | 'path', string>;
-
 export const GET: import('./$types').RequestHandler = () => {
-	const docs = traverse(
-		'docs',
-		({ frontMatter: { title, toc }, content, breadcrumb: [filename] }) => {
-			if (filename.startsWith('draft')) return;
-			content = content.replace(/@VERSION/g, `@${version}`);
-			const index = filename.slice(0, 2);
-			const slug = filename.slice(3, -2);
-			return { index, slug, title, toc, content, path: `docs/${filename}.md` };
-		},
-		forge.types<Metadata, Section & Metadata>()
-	);
+	const docs: Array<{
+		title: string;
+		index: string;
+		slug: string;
+		content: string;
+		path: string;
+	}> = traverse({ entry: 'docs' }, ({ breadcrumb: [filename], content, frontMatter }) => {
+		if (filename.startsWith('draft')) return;
+		content = content.replace(/@VERSION/g, `@${version}`);
+		const index = filename.slice(0, 2);
+		const slug = filename.slice(3, -2);
+		return { ...frontMatter, index, slug, content, path: `docs/${filename}.md` };
+	});
 
 	return json(docs);
 };
