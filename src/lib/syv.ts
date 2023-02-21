c828ef1d69ea44d84240cb7aee926c09dc4b79d9
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-02-21 14:18:35 +0800

    syv options

--- src/lib/syv.ts
+++ src/lib/syv.ts
@@ -1,27 +1,32 @@
-import type { ComponentType, SvelteComponent } from 'svelte';
+import type { ComponentConstructorOptions, ComponentType, SvelteComponent } from 'svelte';
 import type { Exposed, LazyComponent } from './types';
 import { ntv } from 'mauss/std';
 
 let component: SvelteComponent;
 let events: Array<() => void> = [];
 
-export function load<T extends SvelteComponent>(loader: LazyComponent<T>, exposed?: Exposed<T>) {
-	loader().then(({ default: Comp }) => mount(Comp, exposed));
+type SyvOptions<T extends SvelteComponent> = Exposed<T> & {
+	'syv:anchor': ComponentConstructorOptions['target'];
+	'syv:intro': ComponentConstructorOptions['intro'];
+};
+
+export function load<T extends SvelteComponent>(loader: LazyComponent<T>, options?: SyvOptions<T>) {
+	loader().then(({ default: Comp }) => mount(Comp, options));
 }
 
-export function mount<T extends SvelteComponent>(Comp: ComponentType<T>, exposed?: Exposed<T>) {
+export function mount<T extends SvelteComponent>(Comp: ComponentType<T>, options?: SyvOptions<T>) {
 	purge(); // destroy here so it keeps the out transition
 
-	exposed = exposed || ({} as NonNullable<typeof exposed>);
-	const props = Object.keys(exposed).filter((k) => !k.startsWith('on:'));
+	options = options || ({ 'syv:intro': true } as NonNullable<typeof options>);
+	const props = Object.keys(options).filter((k) => !k.includes(':'));
 	component = new Comp({
-		intro: true,
-		target: document.body,
+		intro: options['syv:intro'],
+		target: options['syv:anchor'] || document.body,
 		// @ts-expect-error - valid after T exists
-		props: ntv.pick(props)(exposed),
+		props: ntv.pick(props)(options),
 	});
 
-	for (const [k, v] of Object.entries(exposed)) {
+	for (const [k, v] of Object.entries(options)) {
 		if (!k.startsWith('on:')) continue;
 		events.push(component.$on(k, v));
 	}
