306873ed2b5883c78cbc12b3b3efe62cc7d8a61a
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-02-22 14:14:58 +0800

    check folder existence

--- src/lib/icons/build.mjs
+++ src/lib/icons/build.mjs
@@ -3,6 +3,6 @@ import * as fs from 'fs';
 fs.readdirSync('./scripts').forEach(async (path) => {
 	const script = `./scripts/${path}`;
 	if (fs.statSync(script).isDirectory()) return;
-	if (fs.readdirSync(`./${path}`).length) return;
+	if (fs.existsSync(`./${path.split('.')[0]}`)) return;
 	(await import(script)).build();
 });
