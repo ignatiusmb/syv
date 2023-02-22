9c99649198620e0a58d3a8e74cece585398042b9
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-02-22 13:08:03 +0800

    optimize for dev

--- src/lib/icons/build.mjs
+++ src/lib/icons/build.mjs
@@ -3,5 +3,6 @@ import * as fs from 'fs';
 fs.readdirSync('./scripts').forEach(async (path) => {
 	const script = `./scripts/${path}`;
 	if (fs.statSync(script).isDirectory()) return;
+	if (fs.readdirSync(`./${path}`).length) return;
 	(await import(script)).build();
 });
