6ca76d0a37229b5b1e92fab4921279a65f537876
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-02-24 15:12:44 +0800

    update scripts

--- src/lib/icons/build.mjs
+++ src/lib/icons/build.mjs
@@ -2,7 +2,6 @@ import * as fs from 'fs';
 
 fs.readdirSync('./scripts').forEach(async (path) => {
 	const script = `./scripts/${path}`;
-	if (fs.statSync(script).isDirectory()) return;
 	if (fs.existsSync(`./${path.split('.')[0]}`)) return;
 	(await import(script)).build();
 });
--- src/lib/icons/scripts/feather.mjs
+++ src/lib/icons/scripts/feather.mjs
@@ -1,16 +1,17 @@
-import { mkdir, rm, writeFile } from 'fs/promises';
+import * as fsp from 'fs/promises';
 import { icons as feather } from 'feather-icons';
 
 export async function build() {
-	await rm('./feather', { force: true, recursive: true });
-	await mkdir('./feather');
+	await fsp.rm('./feather', { force: true, recursive: true });
+	await fsp.mkdir('./feather');
+
 	const promises = [];
 	for (const kebab in feather) {
 		let data = '// @ts-nocheck\n';
 		for (const [k, v] of Object.entries(feather[kebab])) {
-			data += `export const ${k} = ${JSON.stringify(v)};\n`;
+			data += `export const ${k} = ${JSON.stringify(v)} as const;\n`;
 		}
-		promises.push(writeFile(`./feather/${kebab}.js`, data));
+		promises.push(fsp.writeFile(`./feather/${kebab}.ts`, data));
 	}
 
 	await Promise.all(promises);
