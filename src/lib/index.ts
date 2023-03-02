337602956c5ed211591dc9865d5ef796d43b3e49
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-03-02 16:07:44 +0800

    chore: isolate core components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,17 +1,1 @@
 export * as syv from './syv.js';
-export { default as ButtonLink } from './core/ButtonLink.svelte';
-export { default as Dialog } from './core/Dialog.svelte';
-export { default as GradientBorder } from './core/GradientBorder.svelte';
-export { default as Image } from './core/Image.svelte';
-export { default as LazyLoad } from './core/LazyLoad.svelte';
-export { default as Link } from './core/Link.svelte';
-export { default as Observe } from './core/Observe.svelte';
-export { default as Overlay } from './core/Overlay.svelte';
-export { default as Pagination } from './core/Pagination.svelte';
-export { default as Periodical } from './core/Periodical.svelte';
-export { default as ProgressBar } from './core/ProgressBar.svelte';
-export { default as ScrollTop } from './core/ScrollTop.svelte';
-export { default as SearchBar } from './core/SearchBar.svelte';
-export { default as ThemeSwitcher } from './core/ThemeSwitcher.svelte';
-export { default as Video } from './core/Video.svelte';
-export { default as WeavedImage } from './core/WeavedImage.svelte';
