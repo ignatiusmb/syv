c6f85f0ff291d210c4e8e6215f96b162149f90b2
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-04-03 21:05:49 +0700

    fix: add type attribute to button (#121)

--- src/lib/core/ScrollTop.svelte
+++ src/lib/core/ScrollTop.svelte
@@ -10,6 +10,7 @@
 
 <svelte:window bind:scrollY={y} />
 <button
+	type="button"
 	class:show={y > ($mounted ? document.body.scrollHeight / 3 : y)}
 	class="syv-core-scroll-top {className}"
 	on:click={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}
@@ -24,13 +25,12 @@
 		z-index: 1;
 		bottom: 0;
 		right: 0;
-		width: 1.5em;
-		height: 1.5em;
+		width: 3rem;
+		height: 3rem;
 		display: inline-flex;
 		align-items: center;
 		justify-content: center;
 		border-radius: 50%;
-		font-size: 1.5em;
 		color: var(--theme-primary, #990000);
 		background-color: rgba(0, 0, 0, 0.25);
 		transform: translate(-50%, 100%);
