7955c3afde076e923505e486721365c86cb591bd
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-06-02 12:37:20 +0700

    chore: upgrade dependency major versions (#129)

--- src/routes/docs.json/+server.ts
+++ src/routes/docs.json/+server.ts
@@ -2,19 +2,26 @@ import { json } from '@sveltejs/kit';
 import { traverse } from 'marqua/fs';
 import { version } from '../../../package.json';
 
+interface PageContent {
+	title: string;
+	index: string;
+	slug: string;
+	content: string;
+	path: string;
+}
+
 export const GET: import('./$types').RequestHandler = () => {
-	const docs: Array<{
-		title: string;
-		index: string;
-		slug: string;
-		content: string;
-		path: string;
-	}> = traverse({ entry: 'docs' }, ({ breadcrumb: [filename], content, frontMatter }) => {
+	const docs = traverse({ entry: 'docs' }, ({ breadcrumb: [filename], buffer, parse }) => {
 		if (filename.startsWith('draft')) return;
-		content = content.replace(/@VERSION/g, `@${version}`);
-		const index = filename.slice(0, 2);
-		const slug = filename.slice(3, -2);
-		return { ...frontMatter, index, slug, content, path: `docs/${filename}.md` };
+		const { content, metadata } = parse(buffer.toString());
+		const specified: PageContent = {
+			index: filename.slice(0, 2),
+			title: metadata.title,
+			slug: filename.slice(3, -2),
+			content: content.replace(/@VERSION/g, `@${version}`),
+			path: `docs/${filename}.md`,
+		};
+		return { ...metadata, ...specified };
 	});
 
 	return json(docs);
