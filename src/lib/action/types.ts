5624589d5fac7d2f5c08ad12db7aeca24a441ab7
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-12-19 11:54:29 +0700

    feat: support SvelteKit v2 (#143)

--- src/lib/action/types.ts
+++ src/lib/action/types.ts
@@ -1,4 +1,4 @@
 import type { Action, ActionReturn } from 'svelte/action';
 
-export { Action, ActionReturn as Returned };
+export type { Action, ActionReturn as Returned };
 export interface HTMLAction<T> extends Action<HTMLElement, T> {}
