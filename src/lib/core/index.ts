142aaba6b1c489b99fc2f43debfabff21099b76f
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-06-11 22:30:22 +0700

    lint

--- src/lib/core/index.ts
+++ src/lib/core/index.ts
@@ -1,8 +1,6 @@
 export { default as Dialog } from './Dialog.svelte';
 export { default as Image } from './Image.svelte';
 export { default as LazyLoad } from './LazyLoad.svelte';
-export { default as Observe } from './Observe.svelte';
-export { default as Overlay } from './Overlay.svelte';
 export { default as Periodical } from './Periodical.svelte';
 export { default as ScrollTop } from './ScrollTop.svelte';
 export { default as SearchBar } from './SearchBar.svelte';
