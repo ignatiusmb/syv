2470e3f8caa752c1bbf2a11a0e27c124d77eb541
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-06-12 11:16:23 +0700

    feat: simplify `<Image>` (#149)

--- src/lib/core/Image.svelte
+++ src/lib/core/Image.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Observe from './Observe.svelte';
-	import Overlay from './Overlay.svelte';
 	import { fade } from 'svelte/transition';
 
 	interface Props {
@@ -10,62 +8,29 @@
 		alt?: string;
 		/** image ratio with y/x axis as floating number */
 		ratio?: number;
-		/** lazily load image using intersection observer */
-		lazy?: boolean;
 		/** set object-fit to contain */
 		contain?: boolean;
-		/** enable overlay with default slot */
-		overlay?: boolean;
-		/** position wrapper div as absolute */
-		absolute?: boolean;
 		/** fade in options */
 		transition?: import('svelte/transition').FadeParams;
-		/** class name */
-		class?: string;
-		children: import('svelte').Snippet;
+		children?: import('svelte').Snippet;
 	}
 
 	const {
 		src = '',
 		alt = '',
 		ratio = 9 / 16,
-		lazy = false,
 		contain = false,
-		overlay = false,
-		absolute = false,
 		transition = {},
-		class: className,
 		children,
 	}: Props = $props();
-
-	let show = $state(false);
 </script>
 
-{#snippet image()}
-	<img {src} {alt} in:fade={transition} class:contain />
-	{#if overlay}
-		<Overlay {show}>
+<div style:padding-top="{ratio * 100}%" class="syv-core-image">
+	<img {src} {alt} loading="lazy" in:fade={transition} class:contain />
+	{#if children}
+		<div class="canvas">
 			{@render children()}
-		</Overlay>
-	{/if}
-{/snippet}
-
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<div
-	style:padding-top="{ratio * 100}%"
-	class:absolute
-	class="syv-core-image {className}"
-	onmouseenter={() => (show = true)}
-	onmouseleave={() => (show = false)}
->
-	{#if lazy}
-		<Observe once={true}>
-			{#snippet children(sighted)}
-				{#if sighted}{@render image()}{/if}
-			{/snippet}
-		</Observe>
-	{:else}
-		{@render image()}
+		</div>
 	{/if}
 </div>
 
@@ -73,25 +38,36 @@
 	div {
 		position: relative;
 		border-radius: inherit;
-	}
-	div.absolute {
-		width: 100%;
-		height: 100%;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
-	img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		position: absolute;
-		top: 0;
-		left: 0;
-		border-radius: inherit;
-		text-align: center;
-	}
-	img.contain {
-		object-fit: contain;
+
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			position: absolute;
+			top: 0;
+			left: 0;
+			border-radius: inherit;
+			text-align: center;
+
+			&.contain {
+				object-fit: contain;
+			}
+		}
+
+		.canvas {
+			opacity: 0;
+			width: 100%;
+			height: 100%;
+			position: absolute;
+			top: 0;
+			left: 0;
+			border-radius: inherit;
+			background: rgba(0, 0, 0, 0.5);
+			transition: var(--t-duration, 300ms);
+
+			&:hover {
+				opacity: 1;
+			}
+		}
 	}
 </style>
