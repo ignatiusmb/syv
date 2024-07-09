2a985c749a53cf13e9e85550e22aaec45620ba2d
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-07-09 16:03:50 +0700

    chore: upgrade essentials (#155)

--- src/routes/docs.json/+server.ts
+++ src/routes/docs.json/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { traverse } from 'marqua/fs';
+import { traverse } from 'aubade/compass';
 import { version } from '../../../package.json';
 
 interface PageContent {
@@ -13,15 +13,15 @@ interface PageContent {
 export const prerender = true;
 
 export const GET: import('./$types').RequestHandler = () => {
-	const docs = traverse({ entry: 'docs' }, ({ breadcrumb: [filename], buffer, parse }) => {
+	const docs = traverse('docs').hydrate(({ breadcrumb: [filename], buffer, parse }) => {
 		if (filename.startsWith('draft')) return;
-		const { content, metadata } = parse(buffer.toString());
+		const { body, metadata } = parse(buffer.toString());
 		const specified: PageContent = {
 			index: filename.slice(0, 2),
 			title: metadata.title,
 			slug: filename.slice(3, -2),
-			content: content.replace(/@VERSION/g, `@${version}`),
-			path: `docs/${filename}.md`,
+			content: body.replace(/@VERSION/g, `@${version}`),
+			path: `docs/${filename}`,
 		};
 		return { ...metadata, ...specified };
 	});
