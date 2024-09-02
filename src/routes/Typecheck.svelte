893ed66559afbf7f6b397f2b5edc611b31ad9654
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-09-02 18:51:21 +0700

    breaking: rename `syv` -> `dialog`

---
+++ src/routes/Typecheck.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import LazyLoad from '$lib/core/LazyLoad.svelte';
+</script>
+
+<LazyLoad
+	when={false}
+	files={[() => import('./ExampleDialog.svelte'), () => import('$lib/core/ScrollTop.svelte')]}
+>
+	{#snippet children([
+		Example,
+		Scroll,
+		// @ts-expect-error
+		Yeah,
+	])}
+		<Example required={false} />
+		<Scroll />
+	{/snippet}
+</LazyLoad>
